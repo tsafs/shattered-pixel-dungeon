e29b0ed74b533019313651e8b8ec4ffc9a7b358c
Sebastian Fast <sfast92@gmail.com>
2023-08-11 15:34:49 +0200

    Added auto-loot for some features

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/npcs/Blacksmith.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/npcs/Blacksmith.java
@@ -54,294 +54,295 @@ import com.watabou.utils.Random;
 import java.util.ArrayList;
 
 public class Blacksmith extends NPC {
-	
-	{
-		spriteClass = BlacksmithSprite.class;
 
-		properties.add(Property.IMMOVABLE);
-	}
-	
-	@Override
-	protected boolean act() {
-		if (Dungeon.hero.buff(AscensionChallenge.class) != null){
-			die(null);
-			return true;
-		}
-		if (Dungeon.level.visited[pos] && !Quest.reforged){
-			Notes.add( Notes.Landmark.TROLL );
-		}
-		return super.act();
-	}
-	
-	@Override
-	public boolean interact(Char c) {
-		
-		sprite.turnTo( pos, c.pos );
+    {
+        spriteClass = BlacksmithSprite.class;
 
-		if (c != Dungeon.hero){
-			return true;
-		}
-		
-		if (!Quest.given) {
-			
-			Game.runOnRenderThread(new Callback() {
-				@Override
-				public void call() {
-					GameScene.show( new WndQuest( Blacksmith.this,
-							Quest.alternative ? Messages.get(Blacksmith.this, "blood_1") : Messages.get(Blacksmith.this, "gold_1") ) {
-						
-						@Override
-						public void onBackPressed() {
-							super.onBackPressed();
-							
-							Quest.given = true;
-							Quest.completed = false;
-							Notes.add( Notes.Landmark.TROLL );
-							
-							Pickaxe pick = new Pickaxe();
-							pick.identify();
-							if (pick.doPickUp( Dungeon.hero )) {
-								GLog.i( Messages.capitalize(Messages.get(Dungeon.hero, "you_now_have", pick.name()) ));
-							} else {
-								Dungeon.level.drop( pick, Dungeon.hero.pos ).sprite.drop();
-							}
-						}
-					} );
-				}
-			});
-			
-		} else if (!Quest.completed) {
-			if (Quest.alternative) {
-				
-				Pickaxe pick = Dungeon.hero.belongings.getItem( Pickaxe.class );
-				if (pick == null) {
-					tell( Messages.get(this, "lost_pick") );
-				} else if (!pick.bloodStained) {
-					tell( Messages.get(this, "blood_2") );
-				} else {
-					if (pick.isEquipped( Dungeon.hero )) {
-						pick.cursed = false; //so that it can always be removed
-						pick.doUnequip( Dungeon.hero, false );
-					}
-					pick.detach( Dungeon.hero.belongings.backpack );
-					tell( Messages.get(this, "completed") );
-					
-					Quest.completed = true;
-					Quest.reforged = false;
-					Statistics.questScores[2] = 3000;
-				}
-				
-			} else {
-				
-				Pickaxe pick = Dungeon.hero.belongings.getItem( Pickaxe.class );
-				DarkGold gold = Dungeon.hero.belongings.getItem( DarkGold.class );
-				if (pick == null) {
-					tell( Messages.get(this, "lost_pick") );
-				} else if (gold == null || gold.quantity() < 15) {
-					tell( Messages.get(this, "gold_2") );
-				} else {
-					if (pick.isEquipped( Dungeon.hero )) {
-						pick.doUnequip( Dungeon.hero, false );
-					}
-					pick.detach( Dungeon.hero.belongings.backpack );
-					gold.detachAll( Dungeon.hero.belongings.backpack );
-					tell( Messages.get(this, "completed") );
-					
-					Quest.completed = true;
-					Quest.reforged = false;
-					Statistics.questScores[2] = 3000;
-				}
-				
-			}
-		} else if (!Quest.reforged) {
-			
-			Game.runOnRenderThread(new Callback() {
-				@Override
-				public void call() {
-					GameScene.show( new WndBlacksmith( Blacksmith.this, Dungeon.hero ) );
-				}
-			});
-			
-		} else {
-			
-			tell( Messages.get(this, "get_lost") );
-			
-		}
+        properties.add(Property.IMMOVABLE);
+    }
 
-		return true;
-	}
-	
-	private void tell( String text ) {
-		Game.runOnRenderThread(new Callback() {
-			@Override
-			public void call() {
-				GameScene.show( new WndQuest( Blacksmith.this, text ) );
-			}
-		});
-	}
-	
-	public static String verify( Item item1, Item item2 ) {
-		
-		if (item1 == item2 && (item1.quantity() == 1 && item2.quantity() == 1)) {
-			return Messages.get(Blacksmith.class, "same_item");
-		}
+    @Override
+    protected boolean act() {
+        if (Dungeon.hero.buff(AscensionChallenge.class) != null) {
+            die(null);
+            return true;
+        }
+        if (Dungeon.level.visited[pos] && !Quest.reforged) {
+            Notes.add(Notes.Landmark.TROLL);
+        }
+        return super.act();
+    }
 
-		if (item1.getClass() != item2.getClass()) {
-			return Messages.get(Blacksmith.class, "diff_type");
-		}
-		
-		if (!item1.isIdentified() || !item2.isIdentified()) {
-			return Messages.get(Blacksmith.class, "un_ided");
-		}
-		
-		if (item1.cursed || item2.cursed ||
-				(item1 instanceof Armor && ((Armor) item1).hasCurseGlyph()) ||
-				(item2 instanceof Armor && ((Armor) item2).hasCurseGlyph()) ||
-				(item1 instanceof Weapon && ((Weapon) item1).hasCurseEnchant()) ||
-				(item2 instanceof Weapon && ((Weapon) item2).hasCurseEnchant())) {
-			return Messages.get(Blacksmith.class, "cursed");
-		}
-		
-		if (item1.level() < 0 || item2.level() < 0) {
-			return Messages.get(Blacksmith.class, "degraded");
-		}
-		
-		if (!item1.isUpgradable() || !item2.isUpgradable()) {
-			return Messages.get(Blacksmith.class, "cant_reforge");
-		}
-		
-		return null;
-	}
-	
-	public static void upgrade( Item item1, Item item2 ) {
-		
-		Item first, second;
-		if (item2.trueLevel() > item1.trueLevel()) {
-			first = item2;
-			second = item1;
-		} else {
-			first = item1;
-			second = item2;
-		}
+    @Override
+    public boolean interact(Char c) {
 
-		Sample.INSTANCE.play( Assets.Sounds.EVOKE );
-		ScrollOfUpgrade.upgrade( Dungeon.hero );
-		Item.evoke( Dungeon.hero );
+        sprite.turnTo(pos, c.pos);
 
-		if (second.isEquipped( Dungeon.hero )) {
-			((EquipableItem)second).doUnequip( Dungeon.hero, false );
-		}
-		second.detach( Dungeon.hero.belongings.backpack );
+        if (c != Dungeon.hero) {
+            return true;
+        }
 
-		if (second instanceof Armor){
-			BrokenSeal seal = ((Armor) second).checkSeal();
-			if (seal != null){
-				Dungeon.level.drop( seal, Dungeon.hero.pos );
-			}
-		}
+        if (!Quest.given) {
 
-		//preserves enchant/glyphs if present
-		if (first instanceof Weapon && ((Weapon) first).hasGoodEnchant()){
-			((Weapon) first).upgrade(true);
-		} else if (first instanceof Armor && ((Armor) first).hasGoodGlyph()){
-			((Armor) first).upgrade(true);
-		} else {
-			first.upgrade();
-		}
-		Dungeon.hero.spendAndNext( 2f );
-		Badges.validateItemLevelAquired( first );
-		Item.updateQuickslot();
-		
-		Quest.reforged = true;
-		
-		Notes.remove( Notes.Landmark.TROLL );
-	}
-	
-	@Override
-	public int defenseSkill( Char enemy ) {
-		return INFINITE_EVASION;
-	}
-	
-	@Override
-	public void damage( int dmg, Object src ) {
-		//do nothing
-	}
+            Game.runOnRenderThread(new Callback() {
+                @Override
+                public void call() {
+                    GameScene.show(new WndQuest(Blacksmith.this,
+                            Quest.alternative ? Messages.get(Blacksmith.this, "blood_1")
+                                    : Messages.get(Blacksmith.this, "gold_1")) {
 
-	@Override
-	public boolean add( Buff buff ) {
-		return false;
-	}
-	
-	@Override
-	public boolean reset() {
-		return true;
-	}
+                        @Override
+                        public void onBackPressed() {
+                            super.onBackPressed();
 
-	public static class Quest {
-		
-		private static boolean spawned;
-		
-		private static boolean alternative;
-		private static boolean given;
-		private static boolean completed;
-		private static boolean reforged;
-		
-		public static void reset() {
-			spawned		= false;
-			given		= false;
-			completed	= false;
-			reforged	= false;
-		}
-		
-		private static final String NODE	= "blacksmith";
-		
-		private static final String SPAWNED		= "spawned";
-		private static final String ALTERNATIVE	= "alternative";
-		private static final String GIVEN		= "given";
-		private static final String COMPLETED	= "completed";
-		private static final String REFORGED	= "reforged";
-		
-		public static void storeInBundle( Bundle bundle ) {
-			
-			Bundle node = new Bundle();
-			
-			node.put( SPAWNED, spawned );
-			
-			if (spawned) {
-				node.put( ALTERNATIVE, alternative );
-				node.put( GIVEN, given );
-				node.put( COMPLETED, completed );
-				node.put( REFORGED, reforged );
-			}
-			
-			bundle.put( NODE, node );
-		}
-		
-		public static void restoreFromBundle( Bundle bundle ) {
+                            Quest.given = true;
+                            Quest.completed = false;
+                            Notes.add(Notes.Landmark.TROLL);
 
-			Bundle node = bundle.getBundle( NODE );
-			
-			if (!node.isNull() && (spawned = node.getBoolean( SPAWNED ))) {
-				alternative	=  node.getBoolean( ALTERNATIVE );
-				given = node.getBoolean( GIVEN );
-				completed = node.getBoolean( COMPLETED );
-				reforged = node.getBoolean( REFORGED );
-			} else {
-				reset();
-			}
-		}
-		
-		public static ArrayList<Room> spawn( ArrayList<Room> rooms ) {
-			if (!spawned && Dungeon.depth > 11 && Random.Int( 15 - Dungeon.depth ) == 0) {
-				
-				rooms.add(new BlacksmithRoom());
-				spawned = true;
-				alternative = Random.Int( 2 ) == 0;
-				
-				given = false;
-				
-			}
-			return rooms;
-		}
-	}
+                            Pickaxe pick = new Pickaxe();
+                            pick.identify();
+                            if (pick.doPickUp(Dungeon.hero)) {
+                                GLog.i(Messages.capitalize(Messages.get(Dungeon.hero, "you_now_have", pick.name())));
+                            } else {
+                                Dungeon.level.drop(pick, Dungeon.hero.pos).sprite.drop();
+                            }
+                        }
+                    });
+                }
+            });
+
+        } else if (!Quest.completed) {
+            if (Quest.alternative) {
+
+                Pickaxe pick = Dungeon.hero.belongings.getItem(Pickaxe.class);
+                if (pick == null) {
+                    tell(Messages.get(this, "lost_pick"));
+                } else if (!pick.bloodStained) {
+                    tell(Messages.get(this, "blood_2"));
+                } else {
+                    if (pick.isEquipped(Dungeon.hero)) {
+                        pick.cursed = false; // so that it can always be removed
+                        pick.doUnequip(Dungeon.hero, false);
+                    }
+                    pick.detach(Dungeon.hero.belongings.backpack);
+                    tell(Messages.get(this, "completed"));
+
+                    Quest.completed = true;
+                    Quest.reforged = false;
+                    Statistics.questScores[2] = 3000;
+                }
+
+            } else {
+
+                Pickaxe pick = Dungeon.hero.belongings.getItem(Pickaxe.class);
+                DarkGold gold = Dungeon.hero.belongings.getItem(DarkGold.class);
+                if (pick == null) {
+                    tell(Messages.get(this, "lost_pick"));
+                } else if (gold == null || gold.quantity() < 15) {
+                    tell(Messages.get(this, "gold_2"));
+                } else {
+                    if (pick.isEquipped(Dungeon.hero)) {
+                        pick.doUnequip(Dungeon.hero, false);
+                    }
+                    pick.detach(Dungeon.hero.belongings.backpack);
+                    gold.detachAll(Dungeon.hero.belongings.backpack);
+                    tell(Messages.get(this, "completed"));
+
+                    Quest.completed = true;
+                    Quest.reforged = false;
+                    Statistics.questScores[2] = 3000;
+                }
+
+            }
+        } else if (!Quest.reforged) {
+
+            Game.runOnRenderThread(new Callback() {
+                @Override
+                public void call() {
+                    GameScene.show(new WndBlacksmith(Blacksmith.this, Dungeon.hero));
+                }
+            });
+
+        } else {
+
+            tell(Messages.get(this, "get_lost"));
+
+        }
+
+        return true;
+    }
+
+    private void tell(String text) {
+        Game.runOnRenderThread(new Callback() {
+            @Override
+            public void call() {
+                GameScene.show(new WndQuest(Blacksmith.this, text));
+            }
+        });
+    }
+
+    public static String verify(Item item1, Item item2) {
+
+        if (item1 == item2 && (item1.quantity() == 1 && item2.quantity() == 1)) {
+            return Messages.get(Blacksmith.class, "same_item");
+        }
+
+        if (item1.getClass() != item2.getClass()) {
+            return Messages.get(Blacksmith.class, "diff_type");
+        }
+
+        if (!item1.isIdentified() || !item2.isIdentified()) {
+            return Messages.get(Blacksmith.class, "un_ided");
+        }
+
+        if (item1.cursed || item2.cursed ||
+                (item1 instanceof Armor && ((Armor) item1).hasCurseGlyph()) ||
+                (item2 instanceof Armor && ((Armor) item2).hasCurseGlyph()) ||
+                (item1 instanceof Weapon && ((Weapon) item1).hasCurseEnchant()) ||
+                (item2 instanceof Weapon && ((Weapon) item2).hasCurseEnchant())) {
+            return Messages.get(Blacksmith.class, "cursed");
+        }
+
+        if (item1.level() < 0 || item2.level() < 0) {
+            return Messages.get(Blacksmith.class, "degraded");
+        }
+
+        if (!item1.isUpgradable() || !item2.isUpgradable()) {
+            return Messages.get(Blacksmith.class, "cant_reforge");
+        }
+
+        return null;
+    }
+
+    public static void upgrade(Item item1, Item item2) {
+
+        Item first, second;
+        if (item2.trueLevel() > item1.trueLevel()) {
+            first = item2;
+            second = item1;
+        } else {
+            first = item1;
+            second = item2;
+        }
+
+        Sample.INSTANCE.play(Assets.Sounds.EVOKE);
+        ScrollOfUpgrade.upgrade(Dungeon.hero);
+        Item.evoke(Dungeon.hero);
+
+        if (second.isEquipped(Dungeon.hero)) {
+            ((EquipableItem) second).doUnequip(Dungeon.hero, false);
+        }
+        second.detach(Dungeon.hero.belongings.backpack);
+
+        if (second instanceof Armor) {
+            BrokenSeal seal = ((Armor) second).checkSeal();
+            if (seal != null) {
+                Dungeon.level.drop(seal, Dungeon.hero.pos, true);
+            }
+        }
+
+        // preserves enchant/glyphs if present
+        if (first instanceof Weapon && ((Weapon) first).hasGoodEnchant()) {
+            ((Weapon) first).upgrade(true);
+        } else if (first instanceof Armor && ((Armor) first).hasGoodGlyph()) {
+            ((Armor) first).upgrade(true);
+        } else {
+            first.upgrade();
+        }
+        Dungeon.hero.spendAndNext(2f);
+        Badges.validateItemLevelAquired(first);
+        Item.updateQuickslot();
+
+        Quest.reforged = true;
+
+        Notes.remove(Notes.Landmark.TROLL);
+    }
+
+    @Override
+    public int defenseSkill(Char enemy) {
+        return INFINITE_EVASION;
+    }
+
+    @Override
+    public void damage(int dmg, Object src) {
+        // do nothing
+    }
+
+    @Override
+    public boolean add(Buff buff) {
+        return false;
+    }
+
+    @Override
+    public boolean reset() {
+        return true;
+    }
+
+    public static class Quest {
+
+        private static boolean spawned;
+
+        private static boolean alternative;
+        private static boolean given;
+        private static boolean completed;
+        private static boolean reforged;
+
+        public static void reset() {
+            spawned = false;
+            given = false;
+            completed = false;
+            reforged = false;
+        }
+
+        private static final String NODE = "blacksmith";
+
+        private static final String SPAWNED = "spawned";
+        private static final String ALTERNATIVE = "alternative";
+        private static final String GIVEN = "given";
+        private static final String COMPLETED = "completed";
+        private static final String REFORGED = "reforged";
+
+        public static void storeInBundle(Bundle bundle) {
+
+            Bundle node = new Bundle();
+
+            node.put(SPAWNED, spawned);
+
+            if (spawned) {
+                node.put(ALTERNATIVE, alternative);
+                node.put(GIVEN, given);
+                node.put(COMPLETED, completed);
+                node.put(REFORGED, reforged);
+            }
+
+            bundle.put(NODE, node);
+        }
+
+        public static void restoreFromBundle(Bundle bundle) {
+
+            Bundle node = bundle.getBundle(NODE);
+
+            if (!node.isNull() && (spawned = node.getBoolean(SPAWNED))) {
+                alternative = node.getBoolean(ALTERNATIVE);
+                given = node.getBoolean(GIVEN);
+                completed = node.getBoolean(COMPLETED);
+                reforged = node.getBoolean(REFORGED);
+            } else {
+                reset();
+            }
+        }
+
+        public static ArrayList<Room> spawn(ArrayList<Room> rooms) {
+            if (!spawned && Dungeon.depth > 11 && Random.Int(15 - Dungeon.depth) == 0) {
+
+                rooms.add(new BlacksmithRoom());
+                spawned = true;
+                alternative = Random.Int(2) == 0;
+
+                given = false;
+
+            }
+            return rooms;
+        }
+    }
 }
